cef0bce9e23e625435a27aacd42734c6b910c4a8
githubusername <azizbekdavronov1110@gmail.com>
2025-03-12 23:38:09 +0500

    Authentication middleware

---
+++ src/middleware/auth.js
@@ -0,0 +1,19 @@
+const jwt = require("jsonwebtoken");
+
+const authMiddleware = (req, res, next) => {
+  const token = req.header("Authorization")?.replace("Bearer ", "");
+  if (!token) return res.status(401).json({ message: "Token not found" });
+
+  try {
+    const decoded = jwt.verify(token, "secret_key");
+    req.user = decoded;
+
+    next();
+  } catch (error) {
+    res.status(401).json({
+      message: "Invalid token",
+    });
+  }
+};
+
+module.exports = authMiddleware;
