7c9cb41fb5afbeb56e53ff26d642ba84d135a547
githubusername <azizbekdavronov1110@gmail.com>
2025-03-12 21:03:35 +0500

    enrollmentModel ready

---
+++ src/models/Enrollment.js
@@ -0,0 +1,21 @@
+const mongoose = require("mongoose");
+
+const enrollmentSchema = new mongoose.Schema(
+  {
+    studentId: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "Student",
+      required: true,
+    },
+    courseId: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "Course",
+      required: true,
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+module.exports = mongoose.model("Enrollment", enrollmentSchema);
