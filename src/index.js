270a67b87442cfb7f4d3817a20c0a1d1fa23a120
githubusername <azizbekdavronov1110@gmail.com>
2025-03-12 23:42:45 +0500

    authRoutes and authMiddleware

--- src/index.js
+++ src/index.js
@@ -14,11 +14,14 @@ const courseRoutes = require("./routes/courses");
 const studentRoutes = require("./routes/students");
 const enrollmentRoutes = require("./routes/enrollments");
 const analyticsRoutes = require("./routes/analytics");
+const authRoutes = require("./routes/auth");
+const authMiddleware = require("./middleware/auth");
 
-app.use("/api/courses", courseRoutes);
-app.use("/api/students", studentRoutes);
-app.use("/api/enrollments", enrollmentRoutes);
-app.use("/api/analytics", analyticsRoutes);
+app.use("/api/auth", authRoutes);
+app.use("/api/courses", authMiddleware, courseRoutes);
+app.use("/api/students", authMiddleware, studentRoutes);
+app.use("/api/enrollments", authMiddleware, enrollmentRoutes);
+app.use("/api/analytics", authMiddleware, analyticsRoutes);
 
 const PORT = process.env.PORT || 8000;
 
