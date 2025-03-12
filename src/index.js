6380a857c963dd042519f8be600743a0eb53a95b
githubusername <azizbekdavronov1110@gmail.com>
2025-03-12 17:38:33 +0500

    MongoDB connected

--- src/index.js
+++ src/index.js
@@ -1,15 +1,17 @@
 const express = require("express");
-const mongoose = require("mongoose");
 const dotenv = require("dotenv");
+const connectDB = require("./config/db");
 
 dotenv.config();
 
 const app = express();
+
+connectDB();
 
 app.use(express.json());
 
 const PORT = process.env.PORT || 8000;
 
 app.listen(PORT, () => {
-  console.log(`The server is running on port ${PORT}`);
+  console.log(`The server is running on port ${PORT} 🚀`);
 });
